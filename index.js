0976f0b6919b1cdaaf0ec953fdd557e035867456
Dennis Heaney <dennis@learningpool.com>
2017-01-20 06:20:02 +0000

    Adjust insult mechanism

--- index.js
+++ index.js
@@ -19,22 +19,21 @@ app.error = function (err, req, res) {
 app.intent( 
 	'sayInsult', {
 		'slots': { 
-			'name' : 'string',
-	   		'insult' : 'string'	
+			'name' : 'string'
 		},
 		'utterances': [
-			'Tell {name} he\'s a {insult}',
-			'Tell {name} he\'s an {insult}',
-			'Tell {name} she\'s a {insult}',
-			'Tell {name} she\'s an {insult}',
-			'Call {name} a {insult}',
-			'Call {name} an {insult}'
+			'{name}'
 		]
 	},
 	function (req, res) {
 		var name = req.slot('name');
-		var insult = req.slot('insult');
-		res.say(name + ', you\'re a ' + insult);
+		var insults = process.env.INSULTS;
+		if (!insults || 0 === insults.length) {
+			insults = ['{name}, you\'re despicable.'];
+		}
+
+		var insult = insults[(Math.floor((Math.random() * insults.length) + 1))];
+		res.say(insult.replace('{name}', name));
 	}	
 );
 
